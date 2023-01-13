62f5f213f7ed37796a1234d2dddc2d24d4ff9b6c
201710787 <egnachio@naver.com>
2023-01-13 14:19:50 +0900

    FEAT: head page modularization

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '소소식탁' });
 });
 
 module.exports = router;
