f953faf6102a68f8417ef4ae3b22e6779b2beb4f
201710787 <egnachio@naver.com>
2023-01-13 17:13:49 +0900

    REFACTOR: index page refactor

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '소소식탁' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: '소소식탁',
+    contents: [
+      {
+        title: '나의 음식 MBTI',
+        desc: '음식으로 알아보는 나의 성격유형',
+        link: '/mbti'
+      },
+      {
+        title: '음식 취향',
+        desc: '나의 음식 취향을 기록하고 공유해보세요',
+        link: '/taste'
+      },
+      {
+        title: '평균',
+        desc: '나의 음식 성향을 대한민국 평균과 비교해보세요',
+        link: '/average'
+      },
+    ]});
 });
 
 module.exports = router;
