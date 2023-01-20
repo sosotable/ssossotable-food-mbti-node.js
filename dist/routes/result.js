4fd76f9454a827ff24bb1019d5038a2eac96cab0
hyunjo01 <hyunjo10217@gmail.com>
2023-01-21 05:51:00 +0900

    FEAT: mbti result

--- dist/routes/result.js
+++ dist/routes/result.js
@@ -5,11 +5,114 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 // @ts-ignore
 const router = express.Router();
-router.route('/')
+const resultModels = require('../model/result');
+let eipoint = 0;
+let snpoint = 0;
+let ftpoint = 0;
+let pjpoint = 0;
+let mbti;
+let point;
+router.route('/:path')
     .get((req, res) => {
     res.redirect(202, '/');
 })
     .post((req, res) => {
-    req.session.user_id === undefined ? res.redirect('/') : res.render('result', { title: '소소식탁 - 결과' });
+    const path = req.params.path;
+    const result = JSON.parse(req.body.result.replace("\'", ""));
+    for (let i = 0; i++; i < result.length) {
+        if (result[i].type == "EI") {
+            eipoint += result[i].score;
+        }
+        else if (result[i].type == "SN") {
+            snpoint += result[i].score;
+        }
+        else if (result[i].type == "FT") {
+            ftpoint += result[i].score;
+        }
+        else if (result[i].type == "PJ") {
+            pjpoint += result[i].score;
+        }
+    }
+    point = defpoint();
+    mbti = sortResult(point);
+    let mbtiResult = resultModels.mbtiRestList[mbti].name;
+    let descResult = resultModels.mbtiRestList[mbti].desc;
+    req.session.user_id === undefined ? res.redirect('/') : res.render('result', { title: '소소식탁 - 결과', path: path, result: { title: mbtiResult, desc: descResult } });
 });
+function defpoint() {
+    let EI;
+    if (eipoint < 5)
+        EI = "E";
+    else
+        EI = "I";
+    let SN;
+    if (snpoint < 5)
+        SN = "S";
+    else
+        SN = "N";
+    let FT;
+    if (ftpoint < 5)
+        FT = "F";
+    else
+        FT = "T";
+    let PJ;
+    if (pjpoint < 5)
+        PJ = "P";
+    else
+        PJ = "J";
+    return (EI + SN + FT + PJ);
+}
+function sortResult(point) {
+    let num = 0;
+    if (point == "ESTJ") {
+        num = 0;
+    }
+    else if (point == "ISTJ") {
+        num = 1;
+    }
+    else if (point == "ENTJ") {
+        num = 2;
+    }
+    else if (point == "INTJ") {
+        num = 3;
+    }
+    else if (point == "ESFJ") {
+        num = 4;
+    }
+    else if (point == "ISFJ") {
+        num = 5;
+    }
+    else if (point == "ENFJ") {
+        num = 6;
+    }
+    else if (point == "INFJ") {
+        num = 7;
+    }
+    else if (point == "ESTP") {
+        num = 8;
+    }
+    else if (point == "ISTP") {
+        num = 9;
+    }
+    else if (point == "ENTP") {
+        num = 10;
+    }
+    else if (point == "INTP") {
+        num = 11;
+    }
+    else if (point == "ESFP") {
+        num = 12;
+    }
+    else if (point == "ISFP") {
+        num = 13;
+    }
+    else if (point == "ENFP") {
+        num = 14;
+    }
+    else if (point == "INFP") {
+        num = 15;
+    }
+    //console.log(num)
+    return num;
+}
 module.exports = router;
--- model/result.ts
+++ model/result.ts
@@ -1,69 +1,70 @@
+
 module.exports = (()=>{
     return {
         mbtiRestList: [
             {
                 name: '텐동',
-                desc: 'desc1'
+                desc: 'ESTJ'
             },
             {
                 name: '샌드위치',
-                desc: 'desc2'
+                desc: 'ISTJ'
             },
             {
                 name: '스테이크',
-                desc: 'desc3'
+                desc: 'ENTJ'
             },
             {
                 name: '타코야끼',
-                desc: 'desc4'
+                desc: 'INTJ'
             },
             {
                 name: '곱도리탕',
-                desc: 'desc5'
+                desc: 'ESFJ'
             },
             {
                 name: '오믈렛',
-                desc: 'desc6'
+                desc: 'ISFJ'
             },
             {
                 name: '떡볶아',
-                desc: 'desc7'
+                desc: 'ENFJ'
             },
             {
                 name: '꿔바로우',
-                desc: 'desc8'
+                desc: 'INFJ'
             },
             {
                 name: '로제파스타',
-                desc: 'desc9'
+                desc: 'ESTP'
             },
             {
                 name: '스모어쿠키',
-                desc: 'desc10'
+                desc: 'ISTP'
             },
             {
                 name: '마라탕',
-                desc: 'desc11'
+                desc: 'ENTP'
             },
             {
                 name: '버블티',
-                desc: 'desc12'
+                desc: 'INTP'
             },
             {
                 name: '연어초밥',
-                desc: 'desc13'
+                desc: 'ESFP'
             },
             {
                 name: '붕어빵',
-                desc: 'desc14'
+                desc: 'ISFP'
             },
             {
                 name: '고구마치즈돈까스',
-                desc: 'desc15'
+                desc: 'ENFP'
             },
             {
                 name: '수플레 펜케이크',
-                desc: 'desc16'
+                desc: 'INFP'
             }
         ]
     }
